b28fed150e031b97c563868154aff0cf9db240d4
xrounak <rounakkumar13005@gmail.com>
2026-08-21 03:06:11 +0530

    Swap shuffle icon for provided design

--- app/components/icons.js
+++ app/components/icons.js
@@ -108,12 +108,7 @@ export function IconRepeatOne(props) {
 export function IconShuffle(props) {
   return (
     <svg {...base} {...props}>
-      <path d="M3 6h3.5c2 0 3 1 4.3 3" />
-      <path d="M3 18h3.5c2 0 3-1 4.3-3" />
-      <path d="M14 6h3.5c1.2 0 2.2 .5 3 1.4" />
-      <path d="M14 18h3.5c1.2 0 2.2-.5 3-1.4" />
-      <path d="M18 3l3 3-3 3" />
-      <path d="M18 15l3 3-3 3" />
+      <path d="M3.5 13L3.29592 12.0476C2.62895 8.93509 5.00172 6 8.18494 6H19M19 6L16 9M19 6L16 3M20.5 11L20.7041 11.9524C21.3711 15.0649 18.9983 18 15.8151 18H5M5 18L8 15M5 18L8 21" />
     </svg>
   );
 }
